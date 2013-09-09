6e4c5600eafa3d3fbe57cad5ec99d535139758f9
Ali Elderov <ae@neobaum.de>
2013-09-09 15:55:05 +0200

    Revert "init"
    This reverts commit 00403d6e29cd10bdfb44831420f3b7f3c0928bd6.

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,10 @@
-var start =  false,
-	Log = console;
+var start =  false;
 
 
 // Wait for device API libraries to load
-function onLoad() {
     document.addEventListener("deviceready", onDeviceReady, false);
 	document.addEventListener("pause", onPause, false);
 	document.addEventListener("resume", onResume, false);
-}
 
 function onPause(){
 	console.log("device Pause");
@@ -19,7 +16,6 @@ function onResume(){
 
 // device APIs are available
 function onDeviceReady() {
-	//waiting 3 second and do it on background
 	setTimeout(function() {
 		navigator.splashscreen.hide();
 	},3000);
@@ -34,8 +30,42 @@ function onDeviceReady() {
 
 
 
-$(document).ready(function(){
-	console.log(">console");
-	window.console.log(">win.console");
-	LOG.log(">LOG");
-});
+//$(function(){
+//	window.console.log("drin");
+////	alert("s");
+//});
+
+//function onDeviceReady() {
+//
+//setTimeout(function() {
+//// interval timer each 1 second
+//var i = 1,
+//tik = setInterval(function() {
+//		console.log('tik',i);
+//		i++;
+//}, 1000);
+//		//close Splash
+//        navigator.splashscreen.hide();
+//		console.log('close splash');
+//		// remove timer
+//		window.clearInterval(tik);
+//		start = true;
+//}, 5000);
+//
+//	console.log('Received Event: device READY');
+//	console.log('show splash 5 sek');
+//
+//
+//	if (start) {
+//        var parentElement = document.getElementById("deviceready");
+//        var listeningElement = parentElement.querySelector('.listening');
+//        var receivedElement = parentElement.querySelector('.received');
+//        listeningElement.setAttribute('style', 'display:none;');
+//        receivedElement.setAttribute('style', 'display:block;');
+//	} else {
+//		navigator.splashscreen.show();
+//	}
+//}
+
+
+
